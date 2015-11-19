0e01f8f18add5195f0bb9b4c1b88904461289f01
Ivan Zamorano <ivanzamorano82@gmail.com>
2015-11-19 19:19:25 +0200

    started validation in Angular.js

--- public/scripts/js/main.js
+++ public/scripts/js/main.js
@@ -1,152 +1,65 @@
-$(function() {
-    /**
-     * Switches tabs for sign up and login.
-     */
-    $('.tabs a').click(function() {
-        if (!$(this).is('.active_tab')) {
-            $(this).addClass('active-tab').siblings().removeClass('active-tab');
-            var rel = $(this).attr('rel');
-            $(rel).show().parent().siblings('form').find('fieldset').hide();
-        }
-    });
+'use strict';
 
-    /**
-     * Switches between authorization and restore tabs.
-     */
-    $('#fp_button').click(function() {
-        $('.authorization').hide();
-        $('.restore').show();
-    });
+var authApp = angular.module('authApp', []);
 
-    /**
-     * Switches between restore and authorization tabs.
-     */
-    $('#back_login').click(function() {
-        $('.restore').hide();
-        $('.authorization').show();
-    });
-
-    /**
-     * Initiates rules of validations for registration form.
-     * And adds additional field "photo" in post.
-     */
-    $('#signUpForm').formValidate({
-        fio:   ['required', 'alphabet', 'length(30)'],
-        login: ['required', 'alphaNumeric(en)', 'length(20)'],
-        email: ['required', 'email', 'length(100)'],
-        pass: ['required', 'range(6,15)'],
-        repeat_pass: ['required', 'matchWith(pass)']
-    }, {
-        photo: function() {
-            var $dz = $('#drop-zone img');
-            if (!$dz.length) {
-                return '';
-            }
-            return encodeURIComponent($dz.attr('src'));
-        }
-    });
-
-    /**
-     * Initiates rules of validations for login form.
-     */
-    $('#loginForm').formValidate({
-        login: ['required'],
-        pass: ['required']
-    });
-
-    /**
-     * Initiates rules of validations for restore password form.
-     */
-    $('#restoreForm').formValidate({
-        email:['required', 'email']
-    });
-
-    /**
-     * Initiates mask for phone input.
-     */
-    $('input[name="phone"]').maskInput('+__(___)___-__-__');
-
-    /**
-     * Prevents D&D in browser.
-     */
-    $(document).on('drop dragover', function (evt) {
-        evt.stopPropagation();
-        evt.preventDefault();
-    });
-
-    /**
-     * Enters in D&D zone.
-     */
-    $(document).on('dragenter', '.photo-wrapp', function() {
-        $(this).addClass('hover');
-    });
-
-    /**
-     * Leaves in D&D zone.
-     */
-    $(document).on('dragleave', '.photo-wrapp', function() {
-        $(this).removeClass('hover');
-    });
-
-    /**
-     * Object of D&D.
-     */
-    var dropZone = document.getElementById('drop-zone');
-
-    /**
-     * Handler of D&D for dropping files.
-     */
-    dropZone.addEventListener('drop', function(evt) {
-        var j = $(this);
-        evt.stopPropagation();
-        evt.preventDefault();
-        var files = evt.dataTransfer.files;
-
-        var file = files[0];
-
-        var reader = new FileReader();
-
-        /**
-         * Successful downloading of file.
-         * @param event
-         */
-        reader.onload = function(event) {
-
-            var image = event.target.result;
-
-            setTimeout(function() {
-                App.changeImage(j, image);
-            }, 500)
-
-        };
-
-        /**
-         * Start of file downloading.
-         */
-        reader.onloadstart = function() {
-            App.changeImage(j);
-        };
-
-        /**
-         * Checks type and available size of downloading file.
-         */
-        var EM;
-        if (file.type.split('/')[0] != 'image') {
-            EM = new App.ErrorMessage($('#d_d'), 'error-block');
-            EM.show(['Загружаемый файл не является изображением']);
-            return false;
-        }
-        else if (false && file.size > App.options.maxUploadSizeFile * 1024 * 1024) {
-            var mb = +App.options.maxUploadSizeFile;
-            EM = new App.ErrorMessage($('#d_d'), 'error-block');
-            EM.show(['Загружаемый файл превышает 2 MB']);
-            return false;
-        }
-        else{
-            reader.readAsDataURL(file);
-        }
-
-    }, false);
-
-    App.getAllTranslates();
+authApp.config(function($interpolateProvider){
+    $interpolateProvider.startSymbol('[[').endSymbol(']]');
 });
+
+authApp.controller('LoginController', ['$scope', '$http',
+    function($scope, $http) {
+        console.log('init login controller');
+
+        $scope.$watch("login_", function (newValue) {
+            var login = $scope.loginForm.login,
+                errors = [];
+
+            if (login.$error.required) {
+                errors.push('Обязательное поле для заполнения.');
+                $scope.errorMessage = 'Обязательное поле для заполнения';
+            }
+
+            if (login.$error.pattern) {
+                errors.push('Поле должно содержать только буквы [a-z]');
+            }
+            $scope.errorMessage = errors.join('. ')
+        });
+
+        $scope.errorMessage = 'Обязательное поле для заполнения.';
+
+        $scope.fio = '44444';
+        //console.log($scope.signUp.fio);
+
+        //jQuery('#signUp').formValidate({
+        //    'login': ['required', 'alphaNumeric(en)', 'length(100)'],
+        //    'password': ['range(6,20)'],
+        //    'type': ['required']
+        //});
+
+        //$http.get('/api/signup').success(function(data) {
+        //    console.log(data);
+        //});
+
+        $scope.doSignUp = function() {
+            console.log('submit');
+        };
+
+        $scope.xxx = 'xxxxxx';
+    }
+]);
+
+authApp.controller('Ctrl', ['$scope', function($scope) {
+    console.log('asdf');
+    $scope.list = [];
+    $scope.text = 'hello';
+    $scope.submit = function() {
+        if (this.text) {
+            this.list.push(this.text);
+            this.text = '';
+        }
+    };
+}]);
+
+var test = angular.module('testModule', []);
+
+//test.
--- public/scripts/js/main_old.js
+++ public/scripts/js/main_old.js
@@ -1,42 +1,152 @@
-'use strict';
+$(function() {
+    /**
+     * Switches tabs for sign up and login.
+     */
+    $('.tabs a').click(function() {
+        if (!$(this).is('.active_tab')) {
+            $(this).addClass('active-tab').siblings().removeClass('active-tab');
+            var rel = $(this).attr('rel');
+            $(rel).show().parent().siblings('form').find('fieldset').hide();
+        }
+    });
 
-var authApp = angular.module('authApp', []);
+    /**
+     * Switches between authorization and restore tabs.
+     */
+    $('#fp_button').click(function() {
+        $('.authorization').hide();
+        $('.restore').show();
+    });
 
-authApp.config(function($interpolateProvider){
-    $interpolateProvider.startSymbol('[[').endSymbol(']]');
-});
+    /**
+     * Switches between restore and authorization tabs.
+     */
+    $('#back_login').click(function() {
+        $('.restore').hide();
+        $('.authorization').show();
+    });
 
-authApp.controller('SignUpController', ['$scope', '$http',
-    function($scope, $http) {
-        console.log('init signUp controller');
+    /**
+     * Initiates rules of validations for registration form.
+     * And adds additional field "photo" in post.
+     */
+    $('#signUpForm').formValidate({
+        fio:   ['required', 'alphabet', 'length(30)'],
+        login: ['required', 'alphaNumeric(en)', 'length(20)'],
+        email: ['required', 'email', 'length(100)'],
+        pass: ['required', 'range(6,15)'],
+        repeat_pass: ['required', 'matchWith(pass)']
+    }, {
+        photo: function() {
+            var $dz = $('#drop-zone img');
+            if (!$dz.length) {
+                return '';
+            }
+            return encodeURIComponent($dz.attr('src'));
+        }
+    });
 
-        $scope.$watch("value", function (newValue) {
-            console.log($scope.signUp);
-        });
+    /**
+     * Initiates rules of validations for login form.
+     */
+    $('#loginForm').formValidate({
+        login: ['required'],
+        pass: ['required']
+    });
 
-        $scope.fio = '44444';
-        //console.log($scope.signUp.fio);
+    /**
+     * Initiates rules of validations for restore password form.
+     */
+    $('#restoreForm').formValidate({
+        email:['required', 'email']
+    });
 
-        //jQuery('#signUp').formValidate({
-        //    'login': ['required', 'alphaNumeric(en)', 'length(100)'],
-        //    'password': ['range(6,20)'],
-        //    'type': ['required']
-        //});
+    /**
+     * Initiates mask for phone input.
+     */
+    $('input[name="phone"]').maskInput('+__(___)___-__-__');
 
-        $http.get('/api/signup').success(function(data) {
-            console.log(data);
-        });
+    /**
+     * Prevents D&D in browser.
+     */
+    $(document).on('drop dragover', function (evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+    });
 
-        $scope.doSignUp = function() {
-            console.log('submit');
-            return false;
+    /**
+     * Enters in D&D zone.
+     */
+    $(document).on('dragenter', '.photo-wrapp', function() {
+        $(this).addClass('hover');
+    });
+
+    /**
+     * Leaves in D&D zone.
+     */
+    $(document).on('dragleave', '.photo-wrapp', function() {
+        $(this).removeClass('hover');
+    });
+
+    /**
+     * Object of D&D.
+     */
+    var dropZone = document.getElementById('drop-zone');
+
+    /**
+     * Handler of D&D for dropping files.
+     */
+    dropZone.addEventListener('drop', function(evt) {
+        var j = $(this);
+        evt.stopPropagation();
+        evt.preventDefault();
+        var files = evt.dataTransfer.files;
+
+        var file = files[0];
+
+        var reader = new FileReader();
+
+        /**
+         * Successful downloading of file.
+         * @param event
+         */
+        reader.onload = function(event) {
+
+            var image = event.target.result;
+
+            setTimeout(function() {
+                App.changeImage(j, image);
+            }, 500)
+
         };
 
-        $scope.xxx = 'xxxxxx';
-    }
-]);
+        /**
+         * Start of file downloading.
+         */
+        reader.onloadstart = function() {
+            App.changeImage(j);
+        };
 
+        /**
+         * Checks type and available size of downloading file.
+         */
+        var EM;
+        if (file.type.split('/')[0] != 'image') {
+            EM = new App.ErrorMessage($('#d_d'), 'error-block');
+            EM.show(['Загружаемый файл не является изображением']);
+            return false;
+        }
+        else if (false && file.size > App.options.maxUploadSizeFile * 1024 * 1024) {
+            var mb = +App.options.maxUploadSizeFile;
+            EM = new App.ErrorMessage($('#d_d'), 'error-block');
+            EM.show(['Загружаемый файл превышает 2 MB']);
+            return false;
+        }
+        else{
+            reader.readAsDataURL(file);
+        }
 
-var test = angular.module('testModule', []);
+    }, false);
 
-//test.
+    App.getAllTranslates();
+});
